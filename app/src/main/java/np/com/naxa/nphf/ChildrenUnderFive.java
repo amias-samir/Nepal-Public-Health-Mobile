c1e8815cc0a99528c4ca68e9a0bc2fc42a4ca21b
bakhatisuman <kindsumansh@gmail.com>
2017-03-24 15:44:28 +0545

    all three layoout are ready

---
+++ app/src/main/java/np/com/naxa/nphf/ChildrenUnderFive.java
@@ -0,0 +1,13 @@
+package np.com.naxa.nphf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChildrenUnderFive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_children_under_five);
+    }
+}
---
+++ app/src/main/java/np/com/naxa/nphf/activities/LactatingWomenActivity.java
@@ -0,0 +1,16 @@
+package np.com.naxa.nphf.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import np.com.naxa.nphf.R;
+
+
+public class LactatingWomenActivity extends AppCompatActivity  {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lactating_women);
+    }
+}
